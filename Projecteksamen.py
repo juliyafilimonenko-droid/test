31e08ded25ab09ba8fc179d7bed6d050b808c035
fastfoodcat <augustbeckersaul@gmail.com>
2026-03-23 09:41:41 +0100

    test

--- Projecteksamen.py
+++ Projecteksamen.py
@@ -1,2 +1,6 @@
- #Dodgespil
-import arcade as arc
+import arcade 
+arcade.open_window(1200, 800, "Projecteksamen")
+arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+arcade.start_render()
+arcade.finish_render()
+arcade.run()
