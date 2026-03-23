4739a16f26ea530e604cf921a8807b733e55a53f
Yuliia <juliya.filimonenko@gmail.com>
2026-03-23 09:01:25 +0100

    test

---
+++ Projecteksamen.py
@@ -0,0 +1,6 @@
+import arcade 
+arcade.open_window(1200, 800, "Projecteksamen")
+arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+arcade.start_render()
+arcade.finish_render()
+arcade.run()
